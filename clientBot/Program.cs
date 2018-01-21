0e0875c498688fa8ffca2f5a2ffd841f6aa129b0
Jeff <jeffrey.hardcastle@gmail.com>
2018-01-20 19:05:32 -0600

    Initial setup for clientBot

--- clientBot/Program.cs
+++ clientBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace clientBot
     {
         static void Main(string[] args)
         {
+            RunFirstExample();
+            Console.ReadKey();
+        }
+
+        private static void RunFirstExample()
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = @"E:\Jeff\test";
+            watcher.Created += watcher_Created;
+            watcher.Deleted += watcher_Deleted;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        static void watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File deleted. Name: {0}", e.Name);
+        }
+
+        static void watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File created. Name: {0}", e.Name);
         }
     }
 }
